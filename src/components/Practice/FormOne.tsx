962e0761851a5b78e41d6389fd86760180141ee0
celse13 <honore.rugira@chromaway.com>
2025-04-05 18:03:58 +0200

    modified the component

--- src/components/Practice/FormOne.tsx
+++ src/components/Practice/FormOne.tsx
@@ -4,7 +4,7 @@ import { FormEvent, useState } from "react";
 const FormOne = () => {
   const [person, setPerson] = useState({
     name: '',
-    age: 0
+    age: ''
   })
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
@@ -14,11 +14,11 @@ const FormOne = () => {
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="name" className='form-label'>Name</label>
-          <input onChange={(event) => setPerson({...person, name: event.target.value })} id='name' type="text" className='form-control' />
+          <input onChange={(event) => setPerson({...person, name: event.target.value })} value={person.name} id='name' type="text" className='form-control' />
         </div>
         <div className="mb-3">
           <label htmlFor="age" className="form-label">Age</label>
-          <input onChange={(event) => setPerson({...person, age: parseInt(event.target.value)})} id="age" type="number" className="form-control" />
+          <input onChange={(event) => setPerson({...person, age: parseInt(event.target.value)})} value={person.age} id="age" type="number" className="form-control" />
         </div>
         <button className="btn btn-primary" type="submit">Submit</button>
       </form>
